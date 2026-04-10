8e4fb3f244f9e29c1dedd050efe626bc32bb57d0
GUSTAVO EMANUEL SIM�O BARBOZA <25253956@educ123.sp.senai.br>
2026-04-10 16:54:04 -0300

    atualização do login

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Constru Tech</title>
-    <link rel="stylesheet" href="css/cadastro.css">
+    <link rel="stylesheet" href="css/login.css">
     <link rel="icon" href="imagens/logo.png">
 </head>
 
