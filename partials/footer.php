c484f605b65223abd2c6078d851d86c62cfb4a82
GUSTAVO EMANUEL SIM�O BARBOZA <25253956@educ123.sp.senai.br>
2026-04-14 14:11:51 -0300

    edição do footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,84 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <footer>
-        <div class="main-footer">
-            <div class="row">
-                <div class="col-footer">
-                    <h4>Total</h4>
-                    <nav>
-                        <ul>
-                            <li>Valor Total em estoque: </li>
-                            <li>Quntidade total de produtos: </li>
-                        </ul>
-                    </nav>
-                </div>
-                <div class="col-footer">
-                    <h4>itens</h4>
-
-                    <div class="detalhes">
-                        <details>
-                            <summary>Bruto<samp class="arrow"></samp></summary>
-
-                            <div class="datatlhes-footer">
-                                <p>Cimento</p>
-                                <p>Madeira</p>
-                                <p>Tijolos</p>
-                                <p>Pedras</p>
-                            </div>
-                        </details>
-                    </div>
-
-                    <div class="detalhes">
-                        <details>
-                            <summary>Ferramentas<samp class="arrow"></samp></summary>
-
-                            <div class="datatlhes-footer">
-                                <p>Martelo</p>
-                                <p>Furadeira</p>
-                                <p>Trena</p>
-                                <p>Nível</p>
-                            </div>
-                        </details>
-                    </div>
-
-                    <div class="detalhes">
-                        <details>
-                            <summary>Acabamento<samp class="arrow"></samp></summary>
-
-                            <div class="datatlhes-footer">
-                                <p>Pisos e Paredes</p>
-                                <p>Torneiras</p>
-                                <p>Bancada</p>
-                                <p>Iluminações</p>
-                            </div>
-                        </details>
-                    </div>
-                </div>
-                <div class="col-footer">
-                    <h4>Estatísticas</h4>
-
-                    <nav>
-                        <ul>
-                            <li>Produto mais caro: </li>
-                            <li>Produto mais barato: </li>
-                            <li>Valor médio dos produtos:</li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
         <div class="barra">
-            <div class="logo-footer">
-                <img src="imagens/logo2.png">
-            </div>
+            <img src="imagens/logo2.png" class="logo-footer">
             <p class="paragrafo-footer">© 2026 - Constru Tech</p>
         </div>
     </footer>
 </body>
+
 </html>
